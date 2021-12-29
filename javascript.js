13d168ccdb8719f9cc6d45fb7375528f9238f91d
Marrion <mmacandog7304@sdsu.edu>
2021-12-28 21:27:33 -0800

    updated

--- javascript.js
+++
@@ -1,9 +0,0 @@
-var elementPosition = $('#end').offset();
-
-$(window).scroll(function(){
-        if($(window).scrollBottom() > elementPosition.bottom){
-              $('#end').css('position','fixed').css('bottom','0');
-        } else {
-            $('#end').css('position','static');
-        }    
-});
